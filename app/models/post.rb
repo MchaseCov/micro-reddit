b15d50417ee42b621f2f811127add2ca2679c6ff
Chase Covington <mchasecov@gmail.com>
2021-09-21 23:37:43 -0400

    Comments added to database, first comment added under first user

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 10_000 }
+
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 16 }
   validates :password, presence: true, length: { minimum: 4, maximum: 256 }
+
+  has_many :posts
 end
